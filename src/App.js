8da9ff8f1abae34646877144144b7741df751423
simratoberoi <simratoberoi2006@gmail.com>
2023-06-30 10:47:28 +0530

    dsvg

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ function App() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
   const [country, setCountry] = useState('');
+  const [isLocationVisible, setLocationVisible] = useState(false);
 
   const backgroundImages = [
     'url("/images/1.jpg")',
-    'url("/images/2.jpeg")',
-    'url("/images/3.jpeg")',
-    'url("/images/6.jpeg")',
+    'url("/images/0.jpeg")',
+    'url("/images/3.jpg")',
+    'url("/images/4.jpeg")',
+    'url("/images/5.jpeg")',
+    'url("/images/7.jpeg")',
+    'url("/images/8.jpeg")',
+    'url("/images/9.jpeg")',
   ];
 
   const fetchNewQuote = () => {
@@ -40,42 +45,45 @@ function App() {
   };
 
   const getCurrentLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        fetch(`https://geolocation-db.com/json/${latitude}:${longitude}`)
-          .then((response) => response.json())
-          .then((data) => {
-            setLocation(data.city);
-            setLatitude(data.latitude);
-            setLongitude(data.longitude);
-            setCountry(data.country_name);
-          })
-          .catch((error) => {
-            console.error('Error fetching location:', error);
-          });
-      });
-    }
+
+    const mockLocation = {
+      city: 'Mock City',
+      latitude: 'Mock Latitude',
+      longitude: 'Mock Longitude',
+      country_name: 'Mock Country',
+    };
+
+    setLocation(mockLocation.city);
+    setLatitude(mockLocation.latitude);
+    setLongitude(mockLocation.longitude);
+    setCountry(mockLocation.country_name);
   };
 
   useEffect(() => {
-    fetchNewQuote(); // Fetch initial quote
-    fetchCurrentTime(); // Set initial time
-    getCurrentLocation(); // Fetch location data
+    fetchNewQuote();
+    fetchCurrentTime();
+    getCurrentLocation();
 
-    const quoteIntervalId = setInterval(fetchNewQuote, 600000); // Fetch new quote every 10 minutes
-    const timeIntervalId = setInterval(fetchCurrentTime, 1000); // Update time every second
-    const backgroundIntervalId = setInterval(changeBackground, 600000); // Change background every 10 minutes
+    const quoteIntervalId = setInterval(fetchNewQuote, 600000);
+    const timeIntervalId = setInterval(fetchCurrentTime, 1000);
+    const backgroundIntervalId = setInterval(changeBackground, 600);
 
     return () => {
-      clearInterval(quoteIntervalId); // Clean up quote interval when component unmounts
-      clearInterval(timeIntervalId); // Clean up time interval when component unmounts
-      clearInterval(backgroundIntervalId); // Clean up background interval when component unmounts
+      clearInterval(quoteIntervalId);
+      clearInterval(timeIntervalId);
+      clearInterval(backgroundIntervalId);
     };
   }, []);
 
   const appStyle = {
     background: backgroundImages[backgroundIndex],
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
+  const toggleLocationVisible = () => {
+    setLocationVisible((prevState) => !prevState);
   };
 
   return (
@@ -83,14 +91,21 @@ function App() {
       <div className="clock">
         <h2 id="time">{currentTime}</h2>
       </div>
-      {location && (
-        <div className="location">
-          <h3>Location: {location}</h3>
-          <p>Latitude: {latitude}</p>
-          <p>Longitude: {longitude}</p>
-          <p>Country: {country}</p>
-        </div>
-      )}
+      <div
+        className="location"
+        onMouseEnter={toggleLocationVisible}
+        onMouseLeave={toggleLocationVisible}
+      >
+        <button className="location-button">&#x25BC;</button>
+        {isLocationVisible && (
+          <div className="location-details">
+            <h3>Location: {location}</h3>
+            <p>Latitude: {latitude}</p>
+            <p>Longitude: {longitude}</p>
+            <p>Country: {country}</p>
+          </div>
+        )}
+      </div>
       <div className="quote">
         <h2>{quote}</h2>
         <small>- {author} -</small>
